5ef851114b75cf37fa6404330a9dd9483a722c7d
Larisa Chikovkina <larisa.chikovkina97@gmail.com>
2017-04-10 22:04:55 +0300

    Third test

---
+++ Test3/Test3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test3
+{
+    class Program
+    {
+        class StringCalculatorReturnZerro
+        {
+
+            public string Add(string number)
+            {
+                string s1;
+                s1 = "" + number;
+                return s1;
+            }
+
+            public int Sum(string s)
+            {
+                string[] words = s.Split(new char[] { ',', '\n' });
+                int sum = 0;
+                if (s.Length == 0) return 0;
+                else
+                {
+                    foreach (string a in words)
+                    {
+                        sum += Convert.ToInt32(a);
+                    }
+                    return sum;
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            var calculator = new StringCalculatorReturnZerro();
+            var num = calculator.Add("2\n1,5");
+            var sum = calculator.Sum(num);
+            Console.WriteLine(sum);
+            Console.Read();
+        }
+    }
+}
