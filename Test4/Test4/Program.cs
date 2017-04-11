7475bc4f49d36bae549de20e4075f8888a3e87d1
Larisa Chikovkina <larisa.chikovkina97@gmail.com>
2017-04-11 04:09:50 +0300

    Fourth test

---
+++ Test4/Test4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test4
+{
+    class Program
+    {
+        class StringCalculatorReturnZerro
+        {
+
+            public string Add(string s)
+            {
+                string str = "//";
+                str += s;
+                return str;
+            }
+
+            public string NewStr(string s)
+            {
+                string[] words = s.Split(new char[] { s[3], '\n', s[0], s[1] });
+                
+                string mas = "";
+                foreach(string a in words)
+                {
+                    mas += a;
+                }
+
+                return mas;
+            }
+
+            public int Sum(string s)
+            {
+                
+                int b = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    string sum = "";
+                    sum += s[i];
+                    b += Convert.ToInt32(sum);
+                }
+                
+                //sum = Convert.ToInt16(s[1]);
+                return Convert.ToInt32(b);
+            }
+
+        }
+
+            static void Main(string[] args)
+            {
+            var calculator = new StringCalculatorReturnZerro();
+            var str = calculator.Add("\n:1:2:7:6");
+            var num = calculator.NewStr(str);
+            var sum = calculator.Sum(num);
+            Console.WriteLine(sum);
+            Console.Read();
+            }
+     }   
+ }   
