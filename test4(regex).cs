3651ef3cb023e21c41377e691a10c5135b34d3eb
Larisa Chikovkina <larisa.chikovkina97@gmail.com>
2017-04-11 23:34:39 +0300

    4 test(2)

---
+++ test4(regex).cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Test4
+{
+    class Program
+    {
+        class StringCalculatorSupportDelimiters
+        {
+
+            public string Add(string s)
+            {
+                string str = "//";
+                str += s;
+                return str;
+            }
+
+            public int Summa(string numbers)
+            {
+                string regex = @"[0-9]+";
+                int sum = 0;
+                foreach (Match match in Regex.Matches(numbers, regex))
+                {
+                    sum += Convert.ToInt32(match.Value);
+                    //Console.WriteLine(match.Value);
+                }
+                return sum;
+            }
+
+           
+
+        }
+
+            static void Main(string[] args)
+            {
+
+
+            var calculator = new StringCalculatorSupportDelimiters();
+            var str = calculator.Add("\n1:2");
+            var sum = calculator.Summa(str);
+            Console.WriteLine(sum);
+            Console.Read();
+        }
+     }   
+ }   
